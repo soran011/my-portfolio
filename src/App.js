96fae196d71adb1fd63bb8237f85f988c79d86af
Soran Haruti <haruti.soran@gmail.com>
2021-03-18 00:06:23 -0400

    finalized the nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar/Navbar";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello World</h2>
-    </div>
+    <Navbar />
   );
 }
 
